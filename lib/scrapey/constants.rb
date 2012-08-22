8caf4eb7422d610939bdadfe857180c07ec31a3b
P Guardiario <pguardiario@gmail.com>
2012-08-22 09:50:37 +0800

    small changes

--- lib/scrapey/constants.rb
+++ lib/scrapey/constants.rb
@@ -1,5 +1,5 @@
 module Scrapey
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
   BASEDIR = File.expand_path(File.dirname($0)).gsub(/\/src$/,'')
   URL = "https://github.com/monkeysuffrage/scrapey"
   #ENV['SSL_FILE'] = "#{Gem.dir}/gems/scrapey-#{Scrapey::VERSION}/ssl/cacert.pem"
